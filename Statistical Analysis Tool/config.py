66a078365666f5fc40d05246191709fd44536202
Liam-S-Sweeney <liam.s.sweeney@outlook.com>
2026-02-11 23:32:02 -0500

    Update config.py for data path and remove unused imports
    Refactor data path handling and remove pandas import.

--- Statistical Analysis Tool/config.py
+++ Statistical Analysis Tool/config.py
@@ -1,12 +1,7 @@
-import pandas as pd
-import numpy as np
+from pathlib import Path
 
-# Global Variables
-RAW_DATA_PATH = r'data_files\VT Teen Study Waves 1,2,3,4 merged all youth variables.csv'
-MISSING_CODES = [-99,-999,-9999]
-
-RAW_DF = pd.read_csv(RAW_DATA_PATH)
-NUMERIC_DF = raw_df.apply(pd.to_numeric, errors='coerce').replace(MISSING_CODES, np.nan)
+DATA_PATH = Path.("data_files") / "VT Teen Study Waves 1,2,3,4 merged all youth variables.csv"
+MISSING_CODES = [-99, -999, -9999]
 
 HUE_COL = "CGender_4"
 SIZE_COL = "CAge_4"
